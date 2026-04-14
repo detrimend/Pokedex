bc2650c430e7b1be5f2d855d944a5ef2573f5ed8
Rasmus Duus <342727@via.dk>
2026-04-14 12:33:08 +0200

    Small CSS change. Extracted getTypeColor into it's own util file.

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -1,3 +1,5 @@
+import { getTypeColor } from "../utils/typeColor"
+
 function DetailCard({ pokemon }) {
   return (
     <div
@@ -17,32 +19,6 @@ function DetailCard({ pokemon }) {
       <img src={pokemon.sprites.front_default} alt={pokemon.name} />
     </div>
   )
-}
-
-// Hugget fra https://github.com/justingolden21/pokemon-types-css
-function getTypeColor(type) {
-  const colors = {
-    normal: "#A8A878",
-    fire: "#F08030",
-    water: "#6890F0",
-    electric: "#F8D030",
-    grass: "#78C850",
-    ice: "#98D8D8",
-    fighting: "#C03028",
-    poison: "#A040A0",
-    ground: "#E0C068",
-    flying: "#A890F0",
-    psychic: "#F85888",
-    bug: "#A8B820",
-    rock: "#B8A038",
-    ghost: "#705898",
-    dragon: "#7038F8",
-    dark: "#705848",
-    steel: "#B8B8D0",
-    fairy: "#EE99AC",
-  }
-
-  return colors[type]
 }
 
 export default DetailCard
